44ab6ab50d64fb4c9139c9ca5015ac4d04b26bb9
Thomas Sapp <tpsapp@hotmail.com>
2020-11-13 06:55:02 -0700

    Add project files.

---
+++ Playfair/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Playfair
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.rtbInput = new System.Windows.Forms.RichTextBox();
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.tbKeyword = new System.Windows.Forms.TextBox();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Keyword:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Input Text:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 245);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Output Text:";
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(235, 22);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnEncrypt.TabIndex = 3;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(316, 23);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnDecrypt.TabIndex = 4;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // rtbInput
+            // 
+            this.rtbInput.Location = new System.Drawing.Point(12, 64);
+            this.rtbInput.Name = "rtbInput";
+            this.rtbInput.Size = new System.Drawing.Size(606, 175);
+            this.rtbInput.TabIndex = 2;
+            this.rtbInput.Text = "";
+            // 
+            // rtbOutput
+            // 
+            this.rtbOutput.Location = new System.Drawing.Point(12, 261);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.ReadOnly = true;
+            this.rtbOutput.Size = new System.Drawing.Size(606, 175);
+            this.rtbOutput.TabIndex = 5;
+            this.rtbOutput.Text = "";
+            // 
+            // tbKeyword
+            // 
+            this.tbKeyword.Location = new System.Drawing.Point(12, 25);
+            this.tbKeyword.Name = "tbKeyword";
+            this.tbKeyword.Size = new System.Drawing.Size(217, 20);
+            this.tbKeyword.TabIndex = 1;
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.Location = new System.Drawing.Point(397, 23);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(75, 23);
+            this.btnHelp.TabIndex = 6;
+            this.btnHelp.Text = "Help";
+            this.btnHelp.UseVisualStyleBackColor = true;
+            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 448);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.tbKeyword);
+            this.Controls.Add(this.rtbOutput);
+            this.Controls.Add(this.rtbInput);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.Text = "Playfair Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.RichTextBox rtbInput;
+        private System.Windows.Forms.RichTextBox rtbOutput;
+        private System.Windows.Forms.TextBox tbKeyword;
+        private System.Windows.Forms.Button btnHelp;
+    }
+}
+
---
+++ Playfair/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Playfair
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            if ((tbKeyword.Text != "") && (rtbInput.Text != ""))
+            {
+                string sKey = tbKeyword.Text.ToLower();
+                string sGrid = null;
+                string sAlpha = "abcdefghiklmnopqrstuvwxyz";
+                string sInput = rtbInput.Text.ToLower();
+                string sOutput = "";
+                Regex rgx = new Regex("[^a-z-]");
+
+                sKey = rgx.Replace(sKey, "");
+
+                sKey = sKey.Replace('j', 'i');
+
+                for (int i = 0; i < sKey.Length; i++)
+                {
+                    if ((sGrid == null) || (!sGrid.Contains(sKey[i])))
+                    {
+                        sGrid += sKey[i];
+                    }
+                }
+
+                for (int i = 0; i < sAlpha.Length; i++)
+                {
+                    if (!sGrid.Contains(sAlpha[i]))
+                    {
+                        sGrid += sAlpha[i];
+                    }
+                }
+
+                sInput = rgx.Replace(sInput, "");
+
+                sInput = sInput.Replace('j', 'i');
+
+                for (int i = 0; i < sInput.Length; i += 2)
+                {
+                    if (((i + 1) < sInput.Length) && (sInput[i] == sInput[i + 1]))
+                    {
+                        sInput = sInput.Insert(i + 1, "x");
+                    }
+                }
+
+                if ((sInput.Length % 2) > 0)
+                {
+                    sInput += "x";
+                }
+
+                int iTemp = 0;
+                do
+                {
+                    int iPosA = sGrid.IndexOf(sInput[iTemp]);
+                    int iPosB = sGrid.IndexOf(sInput[iTemp + 1]);
+                    int iRowA = iPosA / 5;
+                    int iColA = iPosA % 5;
+                    int iRowB = iPosB / 5;
+                    int iColB = iPosB % 5;
+
+                    if (iColA == iColB)
+                    {
+                        iPosA += 5;
+                        iPosB += 5;
+                    }
+                    else
+                    {
+                        if (iRowA == iRowB)
+                        {
+                            if (iColA == 4)
+                            {
+                                iPosA -= 4;
+                            }
+                            else
+                            {
+                                iPosA += 1;
+                            }
+                            if (iColB == 4)
+                            {
+                                iPosB -= 4;
+                            }
+                            else
+                            {
+                                iPosB += 1;
+                            }
+                        }
+                        else
+                        {
+                            if (iRowA < iRowB)
+                            {
+                                iPosA -= iColA - iColB;
+                                iPosB += iColA - iColB;
+                            }
+                            else
+                            {
+                                iPosA += iColB - iColA;
+                                iPosB -= iColB - iColA;
+                            }
+                        }
+                    }
+
+                    if (iPosA >= sGrid.Length)
+                    {
+                        iPosA = 0 + (iPosA - sGrid.Length);
+                    }
+
+                    if (iPosB >= sGrid.Length)
+                    {
+                        iPosB = 0 + (iPosB - sGrid.Length);
+                    }
+
+                    if (iPosA < 0)
+                    {
+                        iPosA = sGrid.Length + iPosA;
+                    }
+
+                    if (iPosB < 0)
+                    {
+                        iPosB = sGrid.Length + iPosB;
+                    }
+
+                    sOutput += sGrid[iPosA].ToString() + sGrid[iPosB].ToString();
+
+                    iTemp += 2;
+                } while (iTemp < sInput.Length);
+
+                rtbOutput.Text = sOutput;
+            }
+            else
+            {
+                MessageBox.Show("The Keyword and Input Text fields are required to contain text.");
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            if ((tbKeyword.Text != "") && (rtbInput.Text != ""))
+            {
+                string sKey = tbKeyword.Text.ToLower();
+                string sGrid = null;
+                string sAlpha = "abcdefghiklmnopqrstuvwxyz";
+                string sInput = rtbInput.Text.ToLower();
+                string sOutput = "";
+
+                sKey = sKey.Replace('j', 'i');
+
+                for (int i = 0; i < sKey.Length; i++)
+                {
+                    if ((sGrid == null) || (!sGrid.Contains(sKey[i])))
+                    {
+                        sGrid += sKey[i];
+                    }
+                }
+
+                for (int i = 0; i < sAlpha.Length; i++)
+                {
+                    if (!sGrid.Contains(sAlpha[i]))
+                    {
+                        sGrid += sAlpha[i];
+                    }
+                }
+
+                int iTemp = 0;
+                do
+                {
+                    int iPosA = sGrid.IndexOf(sInput[iTemp]);
+                    int iPosB = sGrid.IndexOf(sInput[iTemp + 1]);
+                    int iRowA = iPosA / 5;
+                    int iColA = iPosA % 5;
+                    int iRowB = iPosB / 5;
+                    int iColB = iPosB % 5;
+
+                    if (iColA == iColB)
+                    {
+                        iPosA -= 5;
+                        iPosB -= 5;
+                    }
+                    else
+                    {
+                        if (iRowA == iRowB)
+                        {
+                            if (iColA == 0)
+                            {
+                                iPosA += 4;
+                            }
+                            else
+                            {
+                                iPosA -= 1;
+                            }
+                            if (iColB == 0)
+                            {
+                                iPosB += 4;
+                            }
+                            else
+                            {
+                                iPosB -= 1;
+                            }
+                        }
+                        else
+                        {
+                            if (iRowA < iRowB)
+                            {
+                                iPosA -= iColA - iColB;
+                                iPosB += iColA - iColB;
+                            }
+                            else
+                            {
+                                iPosA += iColB - iColA;
+                                iPosB -= iColB - iColA;
+                            }
+                        }
+                    }
+
+                    if (iPosA > sGrid.Length)
+                    {
+                        iPosA = 0 + (iPosA - sGrid.Length);
+                    }
+
+                    if (iPosB > sGrid.Length)
+                    {
+                        iPosB = 0 + (iPosB - sGrid.Length);
+                    }
+
+                    if (iPosA < 0)
+                    {
+                        iPosA = sGrid.Length + iPosA;
+                    }
+
+                    if (iPosB < 0)
+                    {
+                        iPosB = sGrid.Length + iPosB;
+                    }
+
+                    sOutput += sGrid[iPosA].ToString() + sGrid[iPosB].ToString();
+
+                    iTemp += 2;
+                } while (iTemp < sInput.Length);
+
+                rtbOutput.Text = sOutput;
+            }
+            else
+            {
+                MessageBox.Show("The Keyword and Input Text fields are required to contain text.");
+            }
+        }
+
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            string sHelp = "How to Encrypt text:\n";
+            sHelp += "1. Enter a keyword in the Keyword field.  All non-alphabetical characters will be removed from this field.\n";
+            sHelp += "2. Enter the text you would like to encrypt in to the Input Text box.  All non-alphabetical characters will be removed from this field and the letter j will be replaced with the letter i.\n";
+            sHelp += "3. Click the Encrypt button.\n";
+            sHelp += "4. The encrypted text will appear in the Output Text box.\n";
+            sHelp += "\n";
+            sHelp += "How to Decrypt text:\n";
+            sHelp += "1. Enter the keyword in the Keyword field.  All non-alphabetical characters will be removed from this field.\n";
+            sHelp += "2. Enter the text you would like to decrypt in to the Input Text box.  All non-alphabetical characters will be removed from this field.\n";
+            sHelp += "3. Click the Decrypt button.\n";
+            sHelp += "4. The decrypted text will appear in the Output Text box.\n";
+            sHelp += "\n";
+            sHelp += "This program was created by Thomas Sapp and is Copyrighted © 2010 by Sappsworld Programming.\n";
+            sHelp += "The source code is available at http://code.sappsworld.com\n";
+            MessageBox.Show(sHelp, "Playfair Converter Help...", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
